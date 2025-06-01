3a941f84ee096219f8f35a416b48c81be7a9142f
Dazcomrev <151546169+Dazcomrev@users.noreply.github.com>
2025-06-01 19:38:58 +0700

    Внесение изменений 01.06.2025 19:38.

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 const pool = new Pool({
-    connectionString: 'postgresql://postgres:123456@db.yvpdbfltdhkgyleomnkz.supabase.co:5432/postgres',//'postgresql://postgres:123456@localhost:5432/EsportInCompetition',
+    connectionString: process.env.DATABASE_URL,//'postgresql://postgres:123456@db.yvpdbfltdhkgyleomnkz.supabase.co:5432/postgres',//'postgresql://postgres:123456@localhost:5432/EsportInCompetition',
     ssl: { rejectUnauthorized: false }
     //process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
     /*user: 'postgres',
